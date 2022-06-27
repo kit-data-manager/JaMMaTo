d5cf8883a24d3e060704399b2cc334876f0327b2
nicolasblumenroehr <86111342+nicolasblumenroehr@users.noreply.github.com>
2022-06-27 12:51:35 +0200

    Update MetadataReader.py

--- MetadataReader.py
+++ MetadataReader.py
@@ -1,5 +1,5 @@
 import os
-from dicomReader import dicomReader
+from dicomReader import DicomReader
 import logging
 
 class MetadataReader:
@@ -7,17 +7,17 @@ class MetadataReader:
     #The class is instantiated by providing the path to either a single document that contains the metadata for mapping, or
     #to a folder that contains multiple corresponding files with metadata.
     def __init__(self, metadataDocumentDirectory):
-        #Arrays for collected metadata objects in case of a multiple files in a folder directory
+        #Arrays for collected metadata objects in case of multiple dicom files in a folder directory.
         self.allDicomSeries=[]
         isFile = os.path.isfile(metadataDocumentDirectory)
         isDirectory = os.path.isdir(metadataDocumentDirectory)
         if isDirectory==True:
             for i in os.listdir(metadataDocumentDirectory):
                 fileName, fileExtension = os.path.splitext(i)
+                # If the file in the directory is of dicom format, the dicomReader class is called.
                 if fileExtension == ".dcm":
                     f = os.path.join(metadataDocumentDirectory, i)
-                    seriesName=i
-                    dicomSeries=dicomReader(f, os.path.dirname(metadataDocumentDirectory), seriesName)
+                    dicomSeries=DicomReader(f, os.path.dirname(metadataDocumentDirectory), fileName)
                     self.allDicomSeries.append(dicomSeries)
                 else:
                     logging.warning("File format is not supported.")
