b94c7564d124954c29bec84215c7349312531615
nicolasblumenroehr <86111342+nicolasblumenroehr@users.noreply.github.com>
2022-06-27 13:03:10 +0200

    Update __main__.py

--- example/__main__.py
+++ example/__main__.py
@@ -1,26 +1,4 @@
-import json
-import os
-import glob
-from MetadataSchemaReader import MetadataSchemaReader
-from MetadataReader import MetadataReader
-from dicomSeriesMap import dicomSeriesMap
-from dicomStudyMap import dicomStudyMap
-from fillSchema import fillSchema
+from executeDicomMapping import ExecuteDicomMapping
 
-jsonSchema = "PATH"
-draftDir=glob.glob("PATH")
-readSchema=MetadataSchemaReader(jsonSchema, draftDir)
-schema=readSchema.searchedSchema
-document=MetadataReader("PATH")
-metadata=document.allDicomSeries
-
-studyMap=dicomStudyMap(metadata[0])
-allSeriesMaps=[]
-for i in metadata:
-    seriesMap=dicomSeriesMap(i)
-    allSeriesMaps.append(seriesMap)
-studyMap.updateMap(seriesArray=allSeriesMaps)
-
-fillSchema=fillSchema()
-filledSchema=fillSchema.fillObject(schema, list(schema.keys()), studyMap)
-print(filledSchema)
+#This call execute the mapping for a dicom Study to a custom MRI schema.
+ExecuteDicomMapping(jsonSchema="PATH", metadata="PATH", draftDir="PATH")
