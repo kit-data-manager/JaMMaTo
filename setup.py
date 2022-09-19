83a0c93f1ce35cb2ddb91b7a00471e1e5a6e2c4b
nicolasblumenroehr <86111342+nicolasblumenroehr@users.noreply.github.com>
2022-09-19 14:38:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,7 @@ setup(
   keywords = ['Schema','Mapping','DICOM','JSON'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pydicom',
-          'jsonschema',
-          'openpyxl'
+          'jsonschema'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
