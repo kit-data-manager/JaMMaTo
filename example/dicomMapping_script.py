7f39d848cb5814722120292c03ec5644a790187e
nicolasblumenroehr <86111342+nicolasblumenroehr@users.noreply.github.com>
2022-09-20 12:46:25 +0200

    Update dicomMapping_script.py

--- example/dicomMapping_script.py
+++ example/dicomMapping_script.py
@@ -1,10 +1,10 @@
 import NEPMetadataMapping.dicomMapping
 import sys
 
-metadata = sys.argv[1]
+map = sys.argv[1]
 
-map = sys.argv[2]
+metadata = sys.argv[2]
 
 mappedMetadata = sys.argv[3]
 
-NEPMetadataMapping.dicomMapping.__init__(metadata, map, mappedMetadata)
+NEPMetadataMapping.dicomMapping.__init__(map, metadata, mappedMetadata)
