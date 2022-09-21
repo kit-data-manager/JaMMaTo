3e2a6a497654c5c8c9a5811c32b497b04db4109f
nicolasblumenroehr <86111342+nicolasblumenroehr@users.noreply.github.com>
2022-09-21 16:55:36 +0200

    Update dicomMapping.py

--- NEPMetadataMapping/dicomMapping.py
+++ NEPMetadataMapping/dicomMapping.py
@@ -1,8 +1,8 @@
 import json
-from metadataSchemaReader import MetadataSchemaReader
-from metadataReader import MetadataReader
-from mapSchema import MapSchema
-from attributeMapping import AttributeMapping
+from NEPMetadataMapping.metadataSchemaReader import MetadataSchemaReader
+from NEPMetadataMapping.metadataReader import MetadataReader
+from NEPMetadataMapping.mapSchema import MapSchema
+from NEPMetadataMapping.attributeMapping import AttributeMapping
 import urllib.request
 import logging
 # This class uses all components of the mapping tool functionality for dicom and executes them consecutively in the proper order.
@@ -10,8 +10,7 @@ import logging
 
 class DicomMapping:
 
-    def __init__(self, metadata, map, mappedMetadata='mappedMetadata.json'):
-
+    def __init__(self, map, metadata, mappedMetadata='mappedMetadata.json'):
         # The first class reads in the schema and the json rafts for validating the schema for versioning.
         with open(map, 'r') as f:
             mapJson = json.load(f)
@@ -40,16 +39,15 @@ class DicomMapping:
             raise Exception('Files are not from the same study')
 
         # This class attribute is a list of all dicom file objects
-        metadata = document.allDicomSeries
+        seriesMetadata = document.allDicomSeries
 
         # The third class instantiates the map for one MRI study, by taking one arbitrary object class of the list that contains all dicom files.
-        # studyMap=DicomStudyMap(metadata[0])
-        studyMap = AttributeMapping(metadata[0], mapJson, "study")
+        studyMap = AttributeMapping(seriesMetadata[0], mapJson, "study")
 
         # The studyMap contains all schema attributes with their values from the MRI study.
         # The fourth class instantiates the maps for all series within the MRI study, one class instance per dicom file.
         allSeriesMaps = []
-        for i in metadata:
+        for i in seriesMetadata:
             seriesMap = AttributeMapping(i, mapJson, "series")
             allSeriesMaps.append(seriesMap)
 
@@ -60,8 +58,7 @@ class DicomMapping:
         # The fith class takes the schema structure and the schema attributes from the first hirarchy, and the studyMap class with the attributes and their values. It then fills the schema structure with the attribute values,
         # according to their position in the schema.
         filledSchema = MapSchema()
-        filledSchema = filledSchema.fillObject(
-            schema, list(schema.keys()), None, studyMap)
+        filledSchema = filledSchema.fillObject(schema, list(schema.keys()), None, studyMap)
 
         # The filled schema is then written to a json document and saved.
         with open(mappedMetadata, 'w') as f:
