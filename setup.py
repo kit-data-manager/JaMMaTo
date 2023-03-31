107c2e1d4e3cb0f12bf16fcd217e9aac20f3ccc9
nicolasblumenroehr <86111342+nicolasblumenroehr@users.noreply.github.com>
2023-03-31 19:32:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
-
-from setuptools import setup
-
-def main() -> None:
-    with open("requirements.txt") as fp:
-        install_requires = fp.read().strip().split("\n")
-
-    metadata = dict(
-        install_requires=install_requires
-    )
-    setup(**metadata)
-
-if __name__ == "__main__":
-    main()
+from distutils.core import setup
+setup(
+  name = 'jammato',         # How you named your package folder (MyLib)
+  packages = ['jammato'],   # Chose the same as "name"
+  version = '1.0',      # Start with a small number and increase it with every change you make
+  license='cc',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'A package for mapping metadata attributes from proprietary file format schemas to a json schema document.',   # Give a short description about your library
+  author = 'Nicolas Blumenroehr',                   # Type in your name
+  author_email = 'nicolas.blumenroehr@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/user/nicolasblumenroehr',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/kit-data-manager/JaMMaTo/archive/refs/tags/3.0.2.tar.gz',    # I explain this later on
+  keywords = ['Schema', 'Mapping', 'DICOM', 'JSON'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+        'pydicom',
+        'pydicom',
+        'jsonschema',
+        'urllib3',
+        'datetime',
+        'typing',
+        'zipfile'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.10'
+  ],
+)
