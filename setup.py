f2fbc15cd0c5e135ac2f5713df967239803d8acd
nicolasblumenroehr <86111342+nicolasblumenroehr@users.noreply.github.com>
2023-03-31 19:32:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Nicolas Blumenroehr',                   # Type in your name
   author_email = 'nicolas.blumenroehr@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/user/nicolasblumenroehr',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/kit-data-manager/JaMMaTo/archive/refs/tags/3.0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/kit-data-manager/JaMMaTo/archive/refs/tags/3.0.3.tar.gz',    # I explain this later on
   keywords = ['Schema', 'Mapping', 'DICOM', 'JSON'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
         'pydicom',
