290f5f7354c7b9a829a946b01fa83584355c6ad0
nicolasblumenroehr <86111342+nicolasblumenroehr@users.noreply.github.com>
2023-03-31 15:26:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 
 from setuptools import setup
 
-with open("requirements.txt") as fp:
-    install_requires = fp.read().strip().split("\n")
+def main() -> None:
+    with open("requirements.txt") as fp:
+        install_requires = fp.read().strip().split("\n")
 
-metadata = dict(
-    install_requires=install_requires,
-)
-
-def setup_package() -> None:
+    metadata = dict(
+        install_requires=install_requires
+    )
     setup(**metadata)
 
 if __name__ == "__main__":
-    setup_package()
+    main()
