abd2aafe60ea63c1ad7b44d88c8b9701726e22f1
nicolasblumenroehr <nicolas.blumenroehr@gmail.com>
2023-09-21 11:12:29 +0200

    New setup

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'jammato',        
   packages = ['jammato'],   
-  version = '1.1',      
+  version = '1.3',      
   license='cc',        
   description = 'A package for mapping metadata attributes from proprietary file format schemas to a json schema document.',
   author = 'Nicolas Blumenroehr',                  
   author_email = 'nicolas.blumenroehr@gmail.com',      
   url = 'https://github.com/user/nicolasblumenroehr',   
-  download_url = 'https://github.com/kit-data-manager/JaMMaTo/archive/refs/tags/3.0.1.tar.gz',    
+  download_url = 'https://github.com/kit-data-manager/JaMMaTo/archive/refs/tags/3.0.2.tar.gz',    
   keywords = ['Schema', 'Mapping', 'DICOM', 'JSON'],   
   install_requires=[           
         'pydicom',
